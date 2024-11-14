ace7e28bc034e16abffc96e014cdb988debd3741
Jacek <59231601+Poegim@users.noreply.github.com>
2024-11-14 19:56:26 +0100

    Design improvments

--- src/index.php
+++ src/index.php
@@ -16,9 +16,9 @@
   </style>
 </head>
 
-<body class="bg-gradient-to-br from-slate-50 to-slate-300 w-full pt-10 flex justify-center pb-8 min-h-screen"
+<body class="bg-gradient-to-br from-slate-50 to-slate-400 w-full pt-4 flex justify-center pb-4 min-h-screen"
   x-data="{list: [], inputValue: '', title: ''}">
-  <div class="max-w-7xl mt-4 rounded-lg bg-gray-200 p-4">
+  <div class="max-w-7xl rounded-lg border-2 border-gray-200 bg-white p-4 shadow-lg">
 
     <div class="w-full flex justify-center">
       <div>
@@ -30,16 +30,16 @@
   <div class=" w-full flex justify-center mt-4">
           <div class="my-auto">Enter list title: </div>
           <input x-model="title" type="text" name="title"
-            class="ml-2 border border-gray-400 rounded px-2 py-1 shadow hover:bg-gray-50">
+            class="ml-2 border border-gray-400 rounded px-2 py-1 shadow-lg hover:bg-gray-50">
       </div>
       <div class="w-full flex justify-center mt-4">
         <div>
           <input x-model="inputValue" type="text" name="inut_add"
-            class="border border-gray-400 rounded px-2 py-1 shadow hover:bg-gray-50">
+            class="border border-gray-400 rounded px-2 py-1 shadow-lg hover:bg-gray-50">
           <button @click="list.push(inputValue); inputValue = '';" type="button"
-            class="text-md px-8 py-1 rounded bg-gradient-to-tr from-green-200 to-blue-400 hover:from-green-300 hover:to-blue-500 hover:border-gray-100 shadow border-2 border-white">Add</button>
+            class="text-md px-8 py-1 rounded bg-gradient-to-tr from-green-200 to-blue-400 hover:from-green-300 hover:to-blue-500 hover:border-gray-100 shadow-lg border-2 border-white">Add</button>
           <button @click="list.pop(); inputValue = '';" type="button"
-            class="text-md px-8 py-1 rounded bg-gradient-to-tr from-red-600 to-violet-500 hover:from-red-700 text-white hover:to-violet-600 hover:border-gray-100 shadow border-2 border-white">Remove</button>
+            class="text-md px-8 py-1 rounded bg-gradient-to-tr from-red-600 to-violet-500 hover:from-red-700 text-white hover:to-violet-600 hover:border-gray-100 shadow-lg border-2 border-white">Remove</button>
         </div>
       </div>
       <div id="printable_div" x-cloak>
@@ -64,7 +64,7 @@
       </div>
       <div class="w-full flex justify-center">
         <button type="button" id="print_btn"
-          class="text-md px-8 py-1 rounded bg-gradient-to-tr from-violet-200 to-orange-400 hover:from-violet-300 hover:to-orange-500 hover:border-gray-100 shadow border-2 border-white mt-8">Print</button>
+          class="text-md px-8 py-1 rounded bg-gradient-to-tr from-violet-200 to-orange-400 hover:from-violet-300 hover:to-orange-500 hover:border-gray-100 shadow-lg border-2 border-white mt-8">Print</button>
       </div>
     </div>
     <script>
